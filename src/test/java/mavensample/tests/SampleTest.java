53d433f1c77180cdb81e9bb5a2f0c7ff3adb88ed
Isaac-Amano <yishakamano2020@gmail.com>
2022-11-26 18:29:45 -0600

    first Commit

---
+++ src/test/java/mavensample/tests/SampleTest.java
@@ -0,0 +1,16 @@
+package mavensample.tests;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void FirstTest() {
+		System.out.println("Testing Maven 1");
+		
+	}
+	@Test
+	public void SecondTest() {
+		System.out.println("Testing Maven 2");
+	}
+}
